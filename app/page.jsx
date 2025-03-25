7670034617f27021b4964da7e7bf534236087bc5
Sunil Das <95643764+sunildas1@users.noreply.github.com>
2025-03-25 22:15:31 +0600

    complete a deepThink sidebar menu

---
+++ app/page.jsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { assets } from "@/assets/assets";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+
+  const [expand, setExpand] = useState(false)
+  const [messages, setMessages] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  return (
+    <div>
+      <div className="flex h-screen">
+        <Sidebar expand={expand} setExpand={setExpand}/>
+          <div className="flex-1 flex flex-col justify-center items-center px-4 pb-8 bg-[#292a2d] text-white relative">
+            <div className="md:hidden w-full absolute px-4 top-6 flex justify-between items-center">
+              <Image onClick={()=> (expand ? setExpand(false) : setExpand(true))} className="rotate-180" src={assets.menu_icon} alt="Not found"/>
+              <Image className="opacity-70" src={assets.chat_icon} alt="Not found"/>
+            </div>
+
+          {messages.length === 0 ? (
+            <>
+             <div className="flex items-center gap-3">
+              <Image className="h-16 w-[22%]" src={assets.logo_icon} alt=""/>
+              <p className="text-2xl font-medium">Hi, I'm DeepThink</p>
+             </div>
+              <p className="text-sm mt-2">How can I help you today?</p>
+            </>
+          ):
+          (
+            <div></div>
+          )
+        }
+
+        {/* prompt box */}
+        <p className="text-xs absolute bottom-1 text-gray-500">AI-generated, for reference only</p>
+
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/Sidebar.jsx
@@ -0,0 +1,58 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Sidebar = ({expand, setExpand}) => {
+  return (
+    <div className={`flex flex-col justify-between pt-7 bg-[#212327] transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64': 'md:w-20 w-0 max-md:overflow-hidden'}`}>
+        <div>
+            <div className={`flex ${expand ? "flex-row gap-10" : "flex-col items-center gap-8"}`}>
+                <Image className={expand ? "w-36" : "w-10"} src={expand ? assets.logo_text : assets.logo_icon} alt='Not found'/>
+                <div onClick={()=> expand ? setExpand(false) : setExpand(true)} className='group flex justify-center items-center relative hover:bg-gray-500/20 transition-all w-9 h-9 duration-300 aspect-square rounded-lg cursor-pointer'>
+                    <Image src={assets.menu_icon} className='md:hidden' alt='Not found'/>
+                    <Image src={expand ? assets.sidebar_close_icon : assets.sidebar_icon} className='hidden md:block w-7' alt='Not found'/>
+                    <div className={`absolute w-max ${expand ? "left-1/2 -translate-x-1/2 top-12" : "-top-12 -left-1"} opacity-0 group-hover:opacity-100 bg-black text-white transition px-3 py-2 rounded-lg text-sm shadow-lg pointer-events-none`}>
+                        {expand ? "Close sidebar" : "Open sidebar"}
+                        <div className={`w-3 h-3 absolute bg-black rotate-45 ${expand ? "left-1/2 -top-1.5 -translate-x-1/2" : "left-4 -bottom-1.5"}`}>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <button className={`flex justify-center items-center mt-8 cursor-pointer ${expand ? " bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5" : "w-9 h-9 mx-auto relative group hover:bg-gray-500/30 rounded-lg"}`}>
+                <Image className={expand ? 'w-6' : 'w-7'} src={expand ? assets.chat_icon : assets.chat_icon_dull} alt='Not found'/>
+                <div className='absolute w-max -top-12 -mr-11 opacity-0 group-hover:opacity-100 bg-black text-white transition px-3 py-2 rounded-lg text-sm shadow-lg pointer-events-none'>
+                    New chat
+                    <div className='w-3 h-3 bg-black absolute rotate-45 left-4 -bottom-1.5'></div>
+                </div>
+                {expand && <p className='text text-white font-medium'>New chat</p>}
+            </button>
+
+            <div className={`mt-8 text-sm text-white/25 ${expand ? "block" : "hidden"}`}>
+                <p className='my-1'>Recents</p>
+                {/* chatLabel */}
+            </div>
+        </div>
+
+        <div>
+            <div className={`flex items-center relative cursor-pointer group ${expand ? "gap-1 text-white/80 text-sm cursor-pointer rounded-lg p-2.5 border border-primary hover:bg-white/10" : "w-10 h-10 mx-auto hover:bg-gray-500/30 rounded-lg"}`}>
+                <Image className={expand ? "w-5" : "w-6.5 mx-auto"} src={expand ? assets.phone_icon : assets.phone_icon_dull} alt='Not found'/>
+                <div className={`absolute -top-60 pb-8 ${!expand && "-right-40"} opacity-0 group-hover:opacity-100 hidden group-hover:block transition`}>
+                    <div className='w-max relative bg-black text-white text-sm p-3 rounded-lg shadow-lg'>
+                        <Image className='w-44' src={assets.qrcode} alt='Not found'/>
+                        <p>Scan to get DeepThink App</p>
+                        <div className={`w-3 h-3 absolute bg-black rotate-45 ${expand ? "right-1/2" : "left-4"} -bottom-1.5`}></div>
+                    </div>
+                </div>
+               {expand && <> <span>Get App</span> <Image src={assets.new_icon} alt='Not found'/> </>}
+            </div>
+            <div className={`flex items-center ${expand ? 'hover:bg-white/10 rounded-lg' : 'justify-center w-full'} gap-3 p-2 mt-2 text-white/60 text-sm cursor-pointer`}>
+                <Image className='w-7' src={assets.profile_icon} alt='Not found'/>
+                {expand && <span>My Profile</span>}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
